5213f8d6afe81269bc2c0a871bcadd81d68d96a0
Juan Matheus de Oliveira Araujo <juanmatheus12@iclou.com>
2023-11-16 23:02:26 -0300

    feat: config experience section

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -4,7 +4,7 @@ import './globals.scss'
 
 const inter = Poppins({ 
   subsets: ['latin'], 
-  weight: ['400', '500', '600'] 
+  weight: ['300', '400', '500', '600'] 
 })
 
 export const metadata: Metadata = {
