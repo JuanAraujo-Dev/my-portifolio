784b7421e562918543988035a00a585aae20ceaf
AraujoDev <137550563+JuanAraujo-Dev@users.noreply.github.com>
2023-11-20 18:46:01 -0300

    next.config.js

--- next.config.js
+++ next.config.js
@@ -1,4 +1,15 @@
-/** @type {import('next').NextConfig} */
-const nextConfig = {}
-
-module.exports = nextConfig
+// next.config.js
+module.exports = {
+  basePath: '/my-portfolio',
+  assetPrefix: '/my-portfolio/',
+  // Adicione outras configurações, se necessário...
+  async redirects() {
+    return [
+      {
+        source: '/_next/:path*',
+        destination: '/my-portfolio/_next/:path*',
+        permanent: true,
+      },
+    ];
+  },
+};
